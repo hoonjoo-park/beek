e092a349958fa2db9009cd25aedf87897b3b7b45
hoonjoo94 <hoonjoo94@naver.com>
2023-11-05 16:13:04 +0900

    feat: markup example screen

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,6 +1,13 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  type TextStyle,
+  type ViewStyle,
+} from 'react-native';
 import { multiply } from 'beek';
 
 export default function App() {
@@ -10,22 +17,52 @@ export default function App() {
     multiply(3, 7).then(setResult);
   }, []);
 
+  const onPressScanButton = () => {
+    console.log(result);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <Text style={styles.title}>Beek</Text>
+
+      <TouchableOpacity style={styles.scanButton} onPress={onPressScanButton}>
+        <Text style={styles.buttonText}>Start Scanning</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+type Style = {
+  container: ViewStyle;
+  title: TextStyle;
+  scanButton: ViewStyle;
+  buttonText: TextStyle;
+};
+
+const styles = StyleSheet.create<Style>({
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#fff',
   },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
+  title: {
+    marginBottom: 20,
+    fontSize: 24,
+    fontWeight: '700',
+  },
+  scanButton: {
+    height: 48,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    backgroundColor: '#ff5500',
+    borderRadius: 8,
+  },
+  buttonText: {
+    fontSize: 17,
+    fontWeight: '500',
+    color: '#fff',
   },
 });
