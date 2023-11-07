5b772cc78366932fe340cbd8263559349b27bea1
hoonjoo94 <hoonjoo94@naver.com>
2023-11-07 23:36:39 +0900

    feat: add AVFoundation to BeekScannerViewController

--- ios/BeekScannerViewController.swift
+++ ios/BeekScannerViewController.swift
@@ -1,7 +1,13 @@
 import UIKit
+import AVFoundation
 
-class BeekScannerViewController: UIViewController {
-    let label = UILabel()
+class BeekScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    var captureSession: AVCaptureSession!
+    var previewLayer: AVCaptureVideoPreviewLayer!
+    
+    let customNavBar = UIView()
+    let closeButton = UIButton()
+    let closeImage = UIImageView(image: UIImage(systemName: "xmark"))
     
     init() {
         super.init(nibName: nil, bundle: nil)
@@ -16,23 +22,87 @@ class BeekScannerViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        captureSession = AVCaptureSession()
+        
+        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else { return }
+        
+        let videoInput: AVCaptureDeviceInput
+        
+        do {
+            videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
+        } catch {
+            return
+        }
+        
+        if (captureSession.canAddInput(videoInput)) {
+            captureSession.addInput(videoInput)
+        } else {
+            failed()
+            return
+        }
+        
+        let metadataOutput = AVCaptureMetadataOutput()
+        
+        if (captureSession.canAddOutput(metadataOutput)) {
+            captureSession.addOutput(metadataOutput)
+            
+            metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            metadataOutput.metadataObjectTypes = [.ean13]
+        } else {
+            failed()
+            return
+        }
+        
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.frame = view.layer.bounds
+        previewLayer.videoGravity = .resizeAspectFill
+        view.layer.addSublayer(previewLayer)
+        
+        captureSession.startRunning()
+        
         configureViewController()
     }
-
+    
     private func configureViewController() {
-        self.view.addSubview(label)
+        [customNavBar].forEach { self.view.addSubview($0)}
+        customNavBar.addSubview(closeButton)
+        closeButton.addSubview(closeImage)
         
-        label.text = "Beek Scanner"
-        label.textAlignment = .center
-        label.translatesAutoresizingMaskIntoConstraints = false
+        closeButton.addTarget(self, action: #selector(dismissViewController), for: .touchUpInside)
+        closeImage.tintColor = .white
+        
+        view.backgroundColor = .black
+        
+        customNavBar.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.5)
+        customNavBar.translatesAutoresizingMaskIntoConstraints = false
+        closeButton.translatesAutoresizingMaskIntoConstraints = false
+        closeImage.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            label.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-            label.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            customNavBar.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            customNavBar.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            customNavBar.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            customNavBar.heightAnchor.constraint(equalToConstant: 50),
+            
+            closeButton.leadingAnchor.constraint(equalTo: customNavBar.leadingAnchor, constant: 20),
+            closeButton.topAnchor.constraint(equalTo: customNavBar.topAnchor),
+            closeButton.bottomAnchor.constraint(equalTo: customNavBar.bottomAnchor),
+            
+            closeImage.centerXAnchor.constraint(equalTo: closeButton.centerXAnchor),
+            closeImage.centerYAnchor.constraint(equalTo: closeButton.centerYAnchor),
         ])
     }
-
-    private func dismissViewController() {
+    
+    @objc private func dismissViewController() {
         self.dismiss(animated: true, completion: nil)
     }
+    
+    func failed() {
+        let alert = UIAlertController(title: "Scanning Failed", message: "Please Try Again.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        
+        present(alert, animated: true)
+        captureSession = nil
+    }
 }
+
